a0fd87de19bc89d0e0f1ee832746eb7d92bbbff7
Nathaniel Richards <46858886+NateTheDev1@users.noreply.github.com>
2021-07-15 01:54:13 -0400

    Release/0.0.1 (#3)
    * Started landing page
    
    * Finished landing
    
    * Release 0.0.1
    
    Co-authored-by: Nathaniel Richards <nathaniel@datacompusa.com>

--- App.tsx
+++ App.tsx
@@ -1,16 +1,50 @@
 import { StatusBar } from 'expo-status-bar'
 import React from 'react'
+import { useState } from 'react'
 import { StyleSheet, View } from 'react-native'
 import { Router } from './Router'
 
+import AppLoading from 'expo-app-loading'
+import { Asset } from 'expo-asset'
+
 export default function App() {
+  const [appLoaded, setAppLoaded] = useState(false)
+
+  const _cacheResourcesAsync = async () => {
+    const images = [
+      require('./assets/images/brand-discord.png'),
+      require('./assets/images/brand-facebook.png'),
+      require('./assets/images/brand-twitter.png'),
+      require('./assets/images/brand-url.png'),
+      require('./assets/images/landing-image.png'),
+      require('./assets/images/landing-logo.png'),
+    ]
+
+    const cacheImages = images.map((image) => {
+      return Asset.fromModule(image).downloadAsync()
+    })
+
+    await Promise.all(cacheImages)
+  }
+
+  if (!appLoaded) {
+    return (
+      <AppLoading
+        startAsync={_cacheResourcesAsync}
+        onFinish={() => setAppLoaded(true)}
+        onError={console.warn}
+      />
+    )
+  }
+
   return (
     <View style={styles.container}>
       <Router />
-      <StatusBar style="auto" />
+      <StatusBar style="light" />
     </View>
   )
 }
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
