495bcffddcedec29e9ee5b561d6b8c574caa1167
Nathaniel Richards <nathaniel@datacompusa.com>
2021-07-20 17:34:03 -0400

    Added initial url logic

--- components/DeepLinkComponent.tsx
+++ components/DeepLinkComponent.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import { useHistory } from "react-router-native";
 import * as Linking from "expo-linking";
+import { useState } from "react";
+import { useEffect } from "react";
 
 export const DeepLinkComponent = () => {
   const history = useHistory();
+  const [initialURL, setInitialURL] = useState("");
 
   Linking.addEventListener("url", (url) => {
     const { path, queryParams } = Linking.parse(url.url);
@@ -11,5 +14,19 @@ export const DeepLinkComponent = () => {
       history.push("/" + path);
     }
   });
+
+  useEffect(() => {
+    if (Linking.getInitialURL() && initialURL.length < 1) {
+      Linking.getInitialURL().then((res) => {
+        if (res) {
+          const { path, queryParams } = Linking.parse(res);
+          if (path) {
+            history.push("/" + path);
+          }
+        }
+      });
+    }
+  }, []);
+
   return <></>;
 };
